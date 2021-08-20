dd04c2e589a53b9acf29623ed1edb12d7d991d7c
AndusDEV <resres2007@gmail.com>
2021-08-20 23:27:13 +0200

    Small Changes
    Added Crystal Bow Recipe, and custom Cutting Bench
    
    Fixed Transparent Leaves

--- src/main/java/pl/blackstone/crystaldimension/block/CrystalLeavesBlock.java
+++ src/main/java/pl/blackstone/crystaldimension/block/CrystalLeavesBlock.java
@@ -6,20 +6,18 @@ import pl.blackstone.crystaldimension.itemgroup.CdTabItemGroup;
 import pl.blackstone.crystaldimension.CrystalDimensionModElements;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.minecraft.world.biome.BiomeColors;
 import net.minecraft.world.World;
-import net.minecraft.world.FoliageColors;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
@@ -34,8 +32,6 @@ public class CrystalLeavesBlock extends CrystalDimensionModElements.ModElement {
 	public static final Block block = null;
 	public CrystalLeavesBlock(CrystalDimensionModElements instance) {
 		super(instance, 15);
-		FMLJavaModLoadingContext.get().getModEventBus().register(new BlockColorRegisterHandler());
-		FMLJavaModLoadingContext.get().getModEventBus().register(new ItemColorRegisterHandler());
 	}
 
 	@Override
@@ -43,29 +39,16 @@ public class CrystalLeavesBlock extends CrystalDimensionModElements.ModElement {
 		elements.blocks.add(() -> new CustomBlock());
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(CdTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-	private static class BlockColorRegisterHandler {
-		@OnlyIn(Dist.CLIENT)
-		@SubscribeEvent
-		public void blockColorLoad(ColorHandlerEvent.Block event) {
-			event.getBlockColors().register((bs, world, pos, index) -> {
-				return world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefault();
-			}, block);
-		}
-	}
 
-	private static class ItemColorRegisterHandler {
-		@OnlyIn(Dist.CLIENT)
-		@SubscribeEvent
-		public void itemColorLoad(ColorHandlerEvent.Item event) {
-			event.getItemColors().register((stack, index) -> {
-				return FoliageColors.getDefault();
-			}, block);
-		}
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutoutMipped());
 	}
-
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.LEAVES).sound(SoundType.SWEET_BERRY_BUSH).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.SWEET_BERRY_BUSH).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
+					.notSolid().setOpaque((bs, br, bp) -> false));
 			setRegistryName("crystal_leaves");
 		}
 
